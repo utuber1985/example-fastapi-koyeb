1dd7b39e979626f3d582d74ab6295017d8e3511c
utuber1985 <111651418+utuber1985@users.noreply.github.com>
2024-09-07 22:26:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ app = FastAPI()
 from Connect import XTSConnect
 import Exception as ex
 root = "https://ttblaze.iifl.com"
+access_token=""
 # login store access token, feedtoken
 # pos ord hold
 # place order
@@ -16,19 +17,30 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/margin")
+def get_margin():
+    global XTS_int
+    str_margin=XTS_int.get_balance()
+    return {"Margin": str_margin}
 
+@app.get("/profile")
+def get_profile():
+    global XTS_int
+    str_profile=XTS_int.get_balance()
+    return {"Margin": str_profile}
 
 @app.get("/login/{api_key}/{api_secret}")
 def login(api_key: str, api_secret: str):
     root = "https://ttblaze.iifl.com"
+    access_token="Check api key secret or try again"
     print(api_key,api_secret,root)
-    XTS_int = XTSConnect(api_key, api_secret, "WebAPI", root)
-    resp = XTS_int.interactive_login()
-    print(resp)
-    access_token = resp["result"]["token"]
-    Client_ID = resp["result"]["userID"]
-    isInvestor=resp["result"]["isInvestorClient"]    
+    try:
+        XTS_int = XTSConnect(api_key, api_secret, "WebAPI", root)
+        resp = XTS_int.interactive_login()
+        print(resp)
+        access_token = resp["result"]["token"]
+        Client_ID = resp["result"]["userID"]
+        isInvestor=resp["result"]["isInvestorClient"]    
+    except Exception as ex:
+        print(f'Error in token generation: {str(ex)}')
     return {"access_token": access_token}
