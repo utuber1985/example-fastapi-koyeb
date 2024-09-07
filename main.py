fbd5e54b950e3338f145d41ba96f9d513ad749a4
utuber1985 <111651418+utuber1985@users.noreply.github.com>
2024-09-07 21:22:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ from typing import Union
 from fastapi import FastAPI
 
 app = FastAPI()
-
+from Connect import XTSConnect
+import Exception as ex
+root = "https://ttblaze.iifl.com"
+# login store access token, feedtoken
+# pos ord hold
+# place order
+# websocket stream (use access and feed tokens) 
 
 @app.get("/")
 def read_root():
@@ -14,4 +20,12 @@ def read_root():
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
-    
+
+@app.get("/login/{api_key}/{api_secret}")
+def login(api_key: str, api_secret: str):
+        XTS_int = XTSConnect(api_key, api_secret, "WebAPI", root)
+        resp = XTS_int.interactive_login()
+        access_token = resp["result"]["token"]
+        Client_ID = resp["result"]["userID"]
+        isInvestor=resp["result"]["isInvestorClient"]    
+    return {"access_token": access_token}
